d46c2c7929064564a84700257a88237ded72c575
SanjanaBhandure <sweetybhandure@gmail.com>
2022-04-28 20:20:50 +0530

    Added: Check Employee is Present or Absent

---
+++ EmployeeWageBuilderOOPsUC1.java
@@ -0,0 +1,20 @@
+package com.Bridgelabz.Day08_EmployeeWageComputationOOPs;
+
+import java.util.Random;
+
+public class EmployeeWageBuilderOOPsUC1 {
+    static int IS_FULL_TIME = 1;
+        public static void empCheck() {
+            Random random = new Random();
+            int empCheck = random.nextInt(2);
+            if (empCheck == IS_FULL_TIME) {
+                System.out.println("Employee is Present");
+            } else {
+                System.out.println("Employee is Absent");
+            }
+    }
+
+    public static void main(String[] args) {
+        EmployeeWageBuilderOOPsUC1.empCheck();
+    }
+}

c2930714ce1cdbfbef6b2251b7ca555e9d34d37a
SanjanaBhandure <sweetybhandure@gmail.com>
2022-04-29 00:52:39 +0530

    Added: Welcome to Employee Wage Computation

---
+++ EmployeeWageBuilderOOPs.java
@@ -0,0 +1,9 @@
+package com.Bridgelabz.Day08_EmployeeWageComputationOOPs;
+
+public class EmployeeWageBuilderOOPs {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee\n" +
+                        "Wage Computation\n" +
+                        "Program on Master Branch");
+    }
+}
